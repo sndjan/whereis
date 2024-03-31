f6af08b392d72e0b11aed490745cbb0ee4e1f52c
Jan <js485@hdm-stuttgart.de>
2024-03-31 22:47:57 +0200

    add: restart option

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import GameCSS from "../modules/Game.module.css";
-import { Button } from "react-bootstrap";
 
 function Game() {
   const [imageUrl, setImageUrl] = useState(
